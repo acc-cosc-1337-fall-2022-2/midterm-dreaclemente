8fccc12bc3dc69f371996c49c59752d9cd163253
dreaclemente <97616958+dreaclemente@users.noreply.github.com>
2022-10-15 23:41:50 -0500

    q2 finish

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,24 @@
 #include "question2.h"
-
-bool test_config()
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<float> get_cookie(int number)
 {
-    return true;
+    float sugar;
+    float butter;
+    float flour;
+    sugar=number*(1.5/48); // give for 48 cookies 1.5 cup is desired
+    //for number of cookies is number*(1/5/48)
+    // cup desired
+    butter=number*(1.0/48); //give for 48 cookies 1.0 cup is desired
+    //for number of cookies is number*(1.0/48)
+    // cup desired
+    flour=number*(2.75/48); //give for 48 cookies 2.75 cup is desired
+    //for number of cookies is number*(2.75/48)
+    //cup desired
+    vector<float> result;
+    result.push_back(sugar); //enter result in result vector
+    result.push_back(butter); //enter result in result vector
+    result.push_back(flour); //enter result in result vector
+    return result; //result result
 }
--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,5 @@
-bool test_config();
+#include "question2.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<float> get_cookie(int number)
