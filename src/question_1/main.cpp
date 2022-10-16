9b9662fc1659f1bc0ea3142aed76db3f29c19c98
dreaclemente <97616958+dreaclemente@users.noreply.github.com>
2022-10-15 20:42:44 -0500

    question 1 andrea clemente

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,27 @@
+#include <iostream>
+#include<time.h>
+int roll_die() {   //function that generates a random number from 1 to 6
+    int value = 1+ rand()%6;  //generate random number
+    return value;       //return value
+    }
 
 int main()
 {
-
-    return 0;
+        std::string input;   //declare variable
+        srand(time(0));     //time variable
+        //testing the function
+        //for(int i=0;i<10;i++)
+            std::cout<<roll_die()<<" ";
+        //main program
+        while(1) {  //run a loop
+        std::cout<<"Enter Y to roll the die, type something other to stop: ";
+        std::cin>>input;     //take input from user
+        if (input=="Y") {   //if user input Y then call the roll_die function
+            std::cout<<"Result = "<<roll_die()<<"\n";    //print output
+            }
+            else {
+                exit(0);    //exit the program
+            }
+        }
+        return 0;
 }
