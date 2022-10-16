7ffb5832659eb255cef805d72638563897fb341a
dreaclemente <97616958+dreaclemente@users.noreply.github.com>
2022-10-15 23:36:25 -0500

    finished

--- src/question_1/question1.h
+++ src/question_1/question1.h
@@ -1,1 +1,6 @@
-bool test_config();
+#include <iostream>
+#include<time.h>
+int roll_die() {   //function that generates a random number from 1 to 6
+    int value = 1+ rand()%6;  //generate random number
+    return value;       //return value
+    }
