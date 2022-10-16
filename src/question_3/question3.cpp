e5d77214ab032882d1d665d90edbb01c099aa2a8
dreaclemente <97616958+dreaclemente@users.noreply.github.com>
2022-10-15 23:44:34 -0500

    q3 finish

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,6 +1,25 @@
 #include "question3.h"
+#include <iostream>
+#include <assert.h>
+using namespace std;
 
-bool test_config()
-{
-    return true;
+//function definition to swap the reference
+void swapByRef(int &x, int &y) {
+    int temp;
+    temp = x; //save the value at address x
+    x = y;      //put y into x
+    y = temp; //put x into y
+
+    return;
+}
+
+//function definition to swap the values
+void swapbyValue(int x, int y) {
+    int temp;
+
+    temp = x; //save the value of x
+    x = y;    //put y into x
+    y = temp; //put x into y
+
+    return;
 }
--- src/question_3/question3.h
+++ src/question_3/question3.h
@@ -1,1 +1,24 @@
-bool test_config();
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+//function definition to swap the reference
+void swapByRef(int &x, int &y) {
+    int temp;
+    temp = x; //save the value at address x
+    x = y;      //put y into x
+    y = temp; //put x into y
+
+    return;
+}
+
+//function definition to swap the values
+void swapbyValue(int x, int y) {
+    int temp;
+
+    temp = x; //save the value of x
+    x = y;    //put y into x
+    y = temp; //put x into y
+
+    return;
+}
