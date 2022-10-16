fd0885c7470142ec53f3194af1ed5731c8f8bb6f
dreaclemente <97616958+dreaclemente@users.noreply.github.com>
2022-10-15 23:31:57 -0500

    question 2 test ac

--- test/question_test_2/question_tests_2.cpp
+++ test/question_test_2/question_tests_2.cpp
@@ -10,3 +10,14 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+
+TEST_CASE("Test get_cookie")
+{
+	vector<float> result = {48.0, 1.5, 1.0, 2.75};
+	vector<float> result = {96.0, 3.0, 2.0, 5.5};
+	vector<float> result = {24.0, 0.75, 0.5, 1.375};
+
+	REQUIRE(get_cookie(48)) == result;
+	REQUIRE(get_cookie(96)) == result;
+	REQUIRE(get_cookie(24)) == result;
+}
